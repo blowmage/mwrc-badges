a1b3090d439f34bdcfc84e38a56e39b878d994bb
Ernie Miller <ernie@erniemiller.org>
2015-03-08 13:37:36 -0700

    ZOMG FREEDOM PATCHING

--- badges.rb
+++ badges.rb
@@ -26,49 +26,47 @@ pdf.font_families.update(
   'Tungsten' => { normal: "#{FONTS}/Tungsten-Medium.ttf" }
 )
 
-data = [
-  {name: 'Mike Moore 1', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 2', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 3', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 4', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 5', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 6', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 7', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 8', twitter: 'blowmage', github: 'blowmage'},
-  {name: 'Mike Moore 9', twitter: 'blowmage', github: 'blowmage'}
-]
+class Numeric
+
+  def inches
+    self * 72
+  end
+  alias :inch :inches
+
+end
 
 def render_badge(pdf, x, y, person)
   pdf.bounding_box(
     [x, y],
-    width: 72 * 4, height: 72 * 3
+    width: 4.inches, height: 3.inches
   ) do
-    pdf.stamp_at 'logo', [x + 72 * 0.5, y - (72 * 0.15)]
+    pdf.stamp_at 'logo', [x + 0.5.inches, y - 0.15.inches]
     pdf.font 'Tungsten'
     pdf.text_box(
       person['name'],
-      size: 42, at: [72 * 0.4, 72 * 2],
-      width: 72 * 3.2, height: 72 * 1.2, align: :center, valign: :center,
+      size: 42, at: [0.4.inches, 2.inches],
+      width: 3.2.inches, height: 1.2.inches, align: :center, valign: :center,
       overflow: :shrink_to_fit, disable_wrap_by_char: true
     )
     pdf.font 'Gotham'
     width = [person['twitter'], person['github']].map {
       |v| pdf.width_of(v.to_s)
     }.max
-    start_at = 72 * 2 - (width.to_f / 2) + 8
+    start_at = 2.inches - (width.to_f / 2) + 8
     if person['twitter']
-      pdf.stamp_at 'twitter', [x + start_at - 16, y - (72 * 2.26)]
+      pdf.stamp_at 'twitter', [x + start_at - 16, y - 2.26.inches]
       pdf.text_box(
         person['twitter'],
-        size: 12, at: [start_at, 72 * 0.74], width: 72 * 2, height: 72 * 0.31
+        size: 12, at: [start_at, 0.74.inches],
+        width: 2.inches, height: 0.31.inches
       )
     end
     if person['github']
       pdf.stamp_at 'github', [x + start_at - 16, y - (72 * 2.56)]
       pdf.text_box(
         person['github'],
-        size: 12, at: [start_at, 72 * 0.44],
-        width: 72 * 2, height: 72 * 0.31
+        size: 12, at: [start_at, 0.44.inches],
+        width: 72 * 2, height: 0.31.inches
       )
     end
   end
@@ -78,16 +76,16 @@ data = CSV.read(ARGV.first, headers: true)
 data.each_slice(6) do |people|
   y = pdf.bounds.height
   people.each_with_index do |person, index|
-    x = index.odd? ? 72 * 4 : 0
+    x = index.odd? ? 4.inches : 0
     render_badge(pdf, x, y, person)
-    y -= 72 * 3 if index.odd?
+    y -= 3.inches if index.odd?
   end
   pdf.start_new_page
   y = pdf.bounds.height
   people.each_with_index do |person, index|
-    x = index.even? ? 72 * 4 : 0
+    x = index.even? ? 4.inches : 0
     render_badge(pdf, x, y, person)
-    y -= 72 * 3 if index.odd?
+    y -= 3.inches if index.odd?
   end
   pdf.start_new_page
 end
